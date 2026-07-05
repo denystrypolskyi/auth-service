ce4ae8a9444f981822838f587280e764236dba08
denystrypolskyi <denystrypolskyi@gmail.com>
2026-07-05 16:13:10 +0200

    Replace deprecated MockBean in auth tests

--- src/test/java/com/example/auth_service/controller/AuthControllerTest.java
+++ src/test/java/com/example/auth_service/controller/AuthControllerTest.java
@@ -10,9 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.example.auth_service.dto.RegisterRequestDTO;
 import com.example.auth_service.jwt.JwtUtil;
@@ -26,10 +26,10 @@ class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AuthUserService authUserService;
 
-    @MockBean
+    @MockitoBean
     private JwtUtil jwtUtil;
 
     @Test
